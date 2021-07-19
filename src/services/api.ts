2cda920361d753d5ad3590039ad4911949fa1fdc
Phil Scott <philscott.dev@gmail.com>
2021-07-18 21:42:09 -0400

    added service and api route for getPVWatt

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,3 @@
-import { ArrayType } from 'enums/ArrayType'
-import { ModuleType } from 'enums/ModuleType'
-import { buildUrl } from 'helpers/url'
 import fetch from 'isomorphic-unfetch'
 
 /**
@@ -18,54 +15,17 @@ export async function fetcher<T>(
   try {
     const { method, headers, body } = options
     const res = await fetch(url.toString(), {
-      headers: {
-        ...headers,
-        'Content-Type': 'application/json',
-        'X-Api-Key': process.env.NEXT_PUBLIC_NREL_API_KEY,
-      },
       method,
-      body: JSON.stringify(body),
+      mode: 'no-cors',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
     })
-
-    return (await res.json()) as T
+    console.log(res)
+    const data = await res.text()
+    console.log(data)
+    return JSON.parse(data) as T
   } catch (err) {
     console.log(err)
   }
-}
-
-interface GetPVWattsOptions {
-  system_capacity: number
-  module_type: ModuleType
-  array_type: ArrayType
-  tilt: number
-  azimuth: number
-  lat: number
-  lon: number
-}
-
-export async function getPVWatts({
-  system_capacity,
-  module_type,
-  array_type,
-  tilt,
-  azimuth,
-  lat,
-  lon,
-}: GetPVWattsOptions) {
-  const url = buildUrl({
-    baseUrl: 'https://developer.nrel.gov/api',
-    path: '/pvwatts/v6',
-    query: {
-      format: 'json',
-      losses: 2, // assuming 2%
-      system_capacity,
-      module_type,
-      array_type,
-      tilt,
-      azimuth,
-      lat,
-      lon,
-    },
-  })
-  return await fetcher(url)
 }
---
+++ src/services/getPVWatts.ts
@@ -0,0 +1,74 @@
+import { fetcher } from './api'
+import { buildUrl } from 'helpers/url'
+import { ArrayType } from 'enums/ArrayType'
+import { ModuleType } from 'enums/ModuleType'
+
+export interface GetPVWattsOptions {
+  system_capacity: number
+  module_type: ModuleType
+  array_type: ArrayType
+  tilt: number
+  azimuth: number
+  lat: number
+  lon: number
+}
+
+export interface GetPVWattsResponse {
+  inputs: {
+    system_capacity: number
+    lat: number
+    lon: number
+    azimuth: number
+    tilt: number
+    array_type: ArrayType
+    module_type: ModuleType
+    losses: number
+  }
+  errors: any[]
+  warnings: any[]
+  version: string
+  ssc_info: {
+    version: number
+    build: string
+  }
+  station_info: {
+    lat: number
+    lon: number
+    elev: number
+    tz: number
+    location: string
+    city: string
+    state: string
+    solar_resource_file: string
+    distance: number
+  }
+  outputs: {
+    ac_monthly: number[]
+    poa_monthly: number[]
+    solrad_monthly: number[]
+    dc_monthly: number[]
+    ac_annual: number
+    solrad_annual: number
+    capacity_factor: number
+  }
+}
+
+export async function getPVWatts(options: GetPVWattsOptions) {
+  const url = buildUrl({
+    baseUrl: `${window.location.protocol}//${window.location.host}`,
+    path: '/api/production',
+    query: {
+      api_key: process.env.NEXT_PUBLIC_NREL_API_KEY,
+      format: 'json',
+      losses: 2, // assuming 2%
+      system_capacity: options.system_capacity,
+      module_type: options.module_type,
+      array_type: options.array_type,
+      tilt: options.tilt,
+      azimuth: options.azimuth,
+      lat: options.lat,
+      lon: options.lon,
+    },
+  })
+  return await fetcher<GetPVWattsResponse>(url)
+}
